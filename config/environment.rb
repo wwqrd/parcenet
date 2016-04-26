b579b26e49f45e1f6794d2f2e753ce4ee6e6d589
Stephen Mckellar <stephen@thingmaker.io>
2016-04-26 10:29:24 +0100

    Don't attempt to use dotenv on production

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 root = File.expand_path('../lib', File.dirname(__FILE__))
 $LOAD_PATH << root
 
-require 'dotenv'
-Dotenv.load
+environment = ENV['RACK_ENV'] || 'development'
+
+if environment != 'production'
+  require 'dotenv'
+  Dotenv.load
+end
